b2ddb3088644a5f28cb4dbf2b4e3faa9803620f4
Ezekia <ezekiaawardk@gmail.com>
2026-04-15 18:01:34 +0300

    Initialize main.tsx with React app entry point

---
+++ apps/web/src/main.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { AppLanding } from "./app/AppLanding";
+import "./styles.css";
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <AppLanding />
+  </React.StrictMode>
+);
